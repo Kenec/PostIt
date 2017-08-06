c9ec2486fc13bc893c649046da5866320e265c97
Kenec <nnamani.kenechukwu@gmail.com>
2017-08-06 14:44:15 +0100

    change config to production

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 import configuration from '../config.json';
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 
 const config = configuration[env];
 
