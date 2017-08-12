db73a61462c8099c9ae9c1adb6038012e425e508
Kenec <you@example.com>
2017-08-12 07:55:37 +0100

    set up api test

--- server/shared/validations/signup.js
+++ server/shared/validations/signup.js
@@ -1,33 +1,56 @@
 import Validator from 'validator';
 import isEmpty from 'lodash/isEmpty';
 
+/**
+ * validateInput - description
+ *
+ * @param  {object} data description
+ * @return {type}      description
+ */
 export default function validateInput(data) {
   const errors = {};
 
-  if (Validator.isEmpty(data.username)) {
-    errors.username = 'Username is required';
+  if (data.username) {
+    if (Validator.isEmpty(data.username)) {
+      errors.username = 'Username is required';
+    }
   }
-  if (Validator.isEmpty(data.phone)) {
-    errors.phone = 'Phone number is required';
+
+  if (data.phone) {
+    if (Validator.isEmpty(data.phone)) {
+      errors.phone = 'Phone number is required';
+    }
   }
-  if (!Validator.isMobilePhone(data.phone, 'any')) {
-    errors.phone = 'Phone number is invalid';
+
+  if (data.phone) {
+    if (!Validator.isMobilePhone(data.phone, 'any')) {
+      errors.phone = 'Phone number is invalid';
+    }
   }
-  if (Validator.isEmpty(data.email)) {
-    errors.email = 'Email is required';
+
+  if (data.email) {
+    if (Validator.isEmpty(data.email)) {
+      errors.email = 'Email is required';
+    }
+    if (!Validator.isEmail(data.email)) {
+      errors.email = 'Email is invalid';
+    }
   }
-  if (!Validator.isEmail(data.email)) {
-    errors.email = 'Email is invalid';
+
+  if (data.password) {
+    if (Validator.isEmpty(data.password)) {
+      errors.password = 'Password is required';
+    }
   }
-  if (Validator.isEmpty(data.password)) {
-    errors.password = 'Password is required';
+  if (data.repassword) {
+    if (Validator.isEmpty(data.repassword)) {
+      errors.confirmPassword = 'Confirm Password';
+    }
+    if (!Validator.equals(data.password, data.repassword)) {
+      errors.confirmPassword = 'Password did not match';
+    }
   }
-  if (Validator.isEmpty(data.repassword)) {
-    errors.confirmPassword = 'Confirm Password';
-  }
-  if (!Validator.equals(data.password, data.repassword)) {
-    errors.confirmPassword = 'Password did not match';
-  }
+
 
   return {
     errors,
--- server/test/controllerTest.js
+++ server/test/controllerTest.js
@@ -1,158 +1,190 @@
-import should from 'should';
+// Require the dev-dependencies
 import supertest from 'supertest';
-import { User } from '../models';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { Users } from '../models';
+import server from '../app';
 
-const server = supertest.agent('http://localhost:3000');
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
 
-describe('API Route Tests: ', () => {
-  describe('User', () => {
-    // before(() => { // Before each test we empty the database
-    //   User.destroy({});
-    // });
+// const server = supertest.agent(app);
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Users', () => {
+  beforeEach((done) => { // Before each test we empty the database
+    Users.sync({ force: true })
+      .then(() => {
+        done(null);
+      })
+      .catch((error) => {
+        done(error);
+      });
   });
 
-  // Test for sign up
-  describe('Signup', () => {
-    it('should return status code 201', (done) => {
-      server
-        .post('/api/user/signup')
-        .send({
-          username: 'kicheal',
-          email: 'kene@gmail.com',
-          password: 'kene' })
-        .expect('Content-type', /json/)
-        .expect(201)
-        .end((err, res) => {
-          res.status.should.equal(400);
-          done();
-        });
+  describe('API Route Tests: ', () => {
+  // Test for sign up staus code to be 201 and res to be object
+    describe('Signup', () => {
+      it('should return status code 201 and  res to be object', (done) => {
+        chai.request(server)
+          .post('/api/user/signup')
+          .send({
+            username: 'kene',
+            email: 'kene@gmail.com',
+            phone: '07038550515',
+            password: 'kene' })
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            done();
+          });
+      });
+      // Test if the parameter returned have an object of message
+      it('should expect parameter of object on successful signup', (done) => {
+        chai.request(server)
+          .post('/api/user/signup')
+          .send({
+            username: 'kene',
+            email: 'kene@gmail.com',
+            phone: '07038550515',
+            password: 'kene' })
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.have.property('message')
+              .eql('User Created successfully');
+            res.body.should.have.property('success')
+              .eql(true);
+            done();
+          });
+      });
     });
   });
+  // // Test for signin
+  // describe('Signin', () => {
+  //   it('should return status code 201', (done) => {
+  //     server
+  //       .post('/api/user/signin')
+  //       .send({
+  //         username: 'Kene',
+  //         password: 'kene'
+  //       })
+  //       .expect('Content-type', /json/)
+  //       .expect(201)
+  //       .end((err, res) => {
+  //         res.status.should.equal(201);
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // // Test for create group
+  // describe('Create Group', () => {
+  //   it('should return status code 201', (done) => {
+  //     server
+  //       .post('/api/group')
+  //       .send({
+  //         groupname: 'Test',
+  //         createdby: 1
+  //       })
+  //       .expect('Content-type', /json/)
+  //       .expect(400)
+  //       .end((err, res) => {
+  //         res.status.should.equal(400);
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // // Test for create user
+  // describe('Add User to Group', () => {
+  //   it('should return status code 201', (done) => {
+  //     server
+  //       .post('/api/group/2')
+  //       .send({
+  //         userid: 1,
+  //       })
+  //       .expect('Content-type', /json/)
+  //       .end((err, res) => {
+  //         res.status.should.equal(404);
+  //         done();
+  //       });
+  //   });
+  // });
 
-  // Test for signin
-  describe('Signin', () => {
-    it('should return status code 201', (done) => {
-      server
-        .post('/api/user/signin')
-        .send({
-          username: 'Kene',
-          password: 'kene'
-        })
-        .expect('Content-type', /json/)
-        .expect(201)
-        .end((err, res) => {
-          res.status.should.equal(201);
-          done();
-        });
-    });
-  });
+  // // Test for List group
+  // describe('List Group', () => {
+  //   it('should return status code 200', (done) => {
+  //     server
+  //       .get('/api/group/1')
+  //       .expect('Content-type', /json/)
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         res.status.should.equal(200);
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // // Test for create message
+  // describe('Send Message', () => {
+  //   it('should return status code 200', (done) => {
+  //     server
+  //       .post('/api/group/1/message')
+  //       .send({
+  //         message: 'Test',
+  //         priority_level: 'critical',
+  //         sentBy: 2,
+  //       })
+  //       .expect('Content-type', /json/)
+  //       .expect(201)
+  //       .end((err, res) => {
+  //         res.status.should.equal(201);
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // // Test for create message
+  // describe('Retrieve Message by Group id', () => {
+  //   it('should return status code 200', (done) => {
+  //     server
+  //       .get('/api/group/1/messages')
+  //       .expect('Content-type', /json/)
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         res.status.should.equal(200);
+  //         done();
+  //       });
+  //   });
+  // });
 
-  // Test for create group
-  describe('Create Group', () => {
-    it('should return status code 201', (done) => {
-      server
-        .post('/api/group')
-        .send({
-          groupname: 'Test',
-          createdby: 1
-        })
-        .expect('Content-type', /json/)
-        .expect(400)
-        .end((err, res) => {
-          res.status.should.equal(400);
-          done();
-        });
-    });
-  });
-
-  // Test for create user
-  describe('Add User to Group', () => {
-    it('should return status code 201', (done) => {
-      server
-        .post('/api/group/2')
-        .send({
-          userid: 1,
-        })
-        .expect('Content-type', /json/)
-        .end((err, res) => {
-          res.status.should.equal(404);
-          done();
-        });
-    });
-  });
-
-  // Test for List group
-  describe('List Group', () => {
-    it('should return status code 200', (done) => {
-      server
-        .get('/api/group/1')
-        .expect('Content-type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          res.status.should.equal(200);
-          done();
-        });
-    });
-  });
-
-  // Test for create message
-  describe('Send Message', () => {
-    it('should return status code 200', (done) => {
-      server
-        .post('/api/group/1/message')
-        .send({
-          message: 'Test',
-          priority_level: 'critical',
-          sentBy: 2,
-        })
-        .expect('Content-type', /json/)
-        .expect(201)
-        .end((err, res) => {
-          res.status.should.equal(201);
-          done();
-        });
-    });
-  });
-
-  // Test for create message
-  describe('Retrieve Message by Group id', () => {
-    it('should return status code 200', (done) => {
-      server
-        .get('/api/group/1/messages')
-        .expect('Content-type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          res.status.should.equal(200);
-          done();
-        });
-    });
-  });
-
-  // Test for create message
-  describe('Retrieve Message by Group id', () => {
-    it('should return status code 200', (done) => {
-      server
-        .get('/api/group/1/messages')
-        .expect('Content-type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          res.status.should.equal(200);
-          done();
-        });
-    });
-  });
-
-  // Test for create message
-  describe('Retrieve all Group', () => {
-    it('should return status code 200', (done) => {
-      server
-        .get('/api/group')
-        .expect('Content-type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          res.status.should.equal(200);
-          done();
-        });
-    });
-  });
+  // // Test for create message
+  // describe('Retrieve Message by Group id', () => {
+  //   it('should return status code 200', (done) => {
+  //     server
+  //       .get('/api/group/1/messages')
+  //       .expect('Content-type', /json/)
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         res.status.should.equal(200);
+  //         done();
+  //       });
+  //   });
+  // });
+  //
+  // // Test for create message
+  // describe('Retrieve all Group', () => {
+  //   it('should return status code 200', (done) => {
+  //     server
+  //       .get('/api/group')
+  //       .expect('Content-type', /json/)
+  //       .expect(200)
+  //       .end((err, res) => {
+  //         res.status.should.equal(200);
+  //         done();
+  //       });
+  //   });
+  // });
 });
